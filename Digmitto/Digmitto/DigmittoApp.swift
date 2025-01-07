dfe6a0efce59e2d7e8eff3374b76de50738275ff
Grzegorz Kulesza <grzegorz.kulesza@zonifero.com>
2025-01-08 00:18:50 +0100

    first commit: Set app config, add icon set.

---
+++ Digmitto/Digmitto/DigmittoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigmittoApp.swift
+//  Digmitto
+//
+//  Created by Grzegorz Kulesza on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct DigmittoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Digmitto/DigmittoTests/DigmittoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DigmittoTests.swift
+//  DigmittoTests
+//
+//  Created by Grzegorz Kulesza on 07/01/2025.
+//
+
+import Testing
+@testable import Digmitto
+
+struct DigmittoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
