2efdb21fda211f841545ecfe8893b1ebbfb69e88
Grzegorz Kulesza <grzegorz.kulesza@zonifero.com>
2025-01-10 22:53:58 +0100

    Add Spanish, English, Polish  5k worlds set.

--- Digmitto/Digmitto/Models/WordStore.swift
+++ Digmitto/Digmitto/Models/WordStore.swift
@@ -1,9 +1,32 @@
-import SwiftUI
+import Foundation
 
+// Example of loading words from a JSON file
 class WordStore: ObservableObject {
-    @Published var words = ["motormotorT", "tabletableB", "gardengardenX"]
+    @Published var words: [String: [String]] = [:]
+    @Published var selectedLanguage: String = "English"
+    
+    init() {
+        loadWords()
+    }
+    
+    func loadWords() {
+        if let url = Bundle.main.url(forResource: "words", withExtension: "json"),
+           let data = try? Data(contentsOf: url) {
+            let decoder = JSONDecoder()
+            if let jsonData = try? decoder.decode([String: [String]].self, from: data) {
+                words = jsonData
+            } else {
+                print("Failed to decode JSON data")
+            }
+        } else {
+            print("Failed to load JSON file")
+        }
+    }
     
     func getRandomWord() -> String {
-        words.randomElement() ?? "motor"
+        guard let languageWords = words[selectedLanguage] else {
+            return "No word"
+        }
+        return languageWords.randomElement() ?? "No word"
     }
-} 
+}
--- Digmitto/Digmitto/Views/SettingsView.swift
+++ Digmitto/Digmitto/Views/SettingsView.swift
@@ -1,32 +1,28 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @EnvironmentObject var wordStore: WordStore
     @Binding var isCheatSheetVisible: Bool
-    @State private var isColoringEnabled = true
-    @State private var selectedLanguage = "English"
-
+    
     var body: some View {
         Form {
-            Section(header: Text("Display Options")) {
-                Toggle("Enable Letter Coloring", isOn: $isColoringEnabled)
-                Toggle("Show Cheat Sheet", isOn: $isCheatSheetVisible)
+            Toggle(isOn: $isCheatSheetVisible) {
+                Text("Enable Cheat Sheet")
             }
-
-            Section(header: Text("Language")) {
-                Picker("Select Language", selection: $selectedLanguage) {
-                    Text("Polish").tag("Polish")
-                    Text("English").tag("English")
-                    Text("Spanish").tag("Spanish")
+            
+            Picker("Language", selection: $wordStore.selectedLanguage) {
+                ForEach(["English", "Polish", "Spanish"], id: \.self) { language in
+                    Text(language).tag(language)
                 }
-                .pickerStyle(SegmentedPickerStyle())
             }
         }
-        .navigationTitle("Settings")
+        .navigationBarTitle("Settings")
     }
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView(isCheatSheetVisible: .constant(true))
+            .environmentObject(WordStore())
     }
 } 
