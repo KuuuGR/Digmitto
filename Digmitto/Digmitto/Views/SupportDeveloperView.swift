2b8ddaa56cbbb0f6a7e574693846d38186eecc9d
Grzegorz Kulesza <grzegorz.kulesza@zonifero.com>
2025-01-25 22:37:30 +0100

    add support paypal logo and emoji ico to emphasize section

--- Digmitto/Digmitto/Views/SupportDeveloperView.swift
+++ Digmitto/Digmitto/Views/SupportDeveloperView.swift
@@ -41,6 +41,33 @@ struct SupportDeveloperView: View {
             }
             .padding(.horizontal)
 
+            
+            Button(action: {
+                withAnimation {
+                    if let url = URL(string: "https://www.paypal.me/etaosin") {
+                        UIApplication.shared.open(url)
+                    }
+                }
+            }) {
+                Image("Paypal_2014_logo")
+                    .resizable()
+                    .scaledToFit() // Ensures the image scales proportionally
+                    .frame(width: 40, height: 40) // Adjust this size to fit the button dimensions
+                    .padding(10) // Slight padding for a better look
+                    .background(.white)
+                    .background(
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color.green.opacity(0.6), Color.blue.opacity(0.6)]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                    )
+                    .cornerRadius(10) // Matches the button's corner radius
+                    .shadow(radius: 5)
+            }
+            .frame(width: 60, height: 60) // The button's overall size
+            .padding(.bottom, 5)
+                        
             Spacer()
         }
         .padding()
